f9b99cb9ae9def7a6f2084e6fc46a24642e84c2c
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-10 19:17:58 -0800

    add routes for photos and cart components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import Header from "./components/header/Header";
+import Photos from "./pages/photos/Photos";
+import Cart from "./pages/cart/Cart";
+import { Routes,Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <h1>Home page</h1>
       <Header/>
+      <h1>Home page</h1>
+
+      <Routes>
+        <Route path="/" element={<Photos/>} />
+        <Route path="/cart" element={<Cart/>} />
+      </Routes>
     </div>
   ) ;
 }
