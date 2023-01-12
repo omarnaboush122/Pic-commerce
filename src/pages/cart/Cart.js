91fd106214dd75105185a5f38e775ed106e19f4f
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-11 17:45:19 -0800

    map over cartitems array

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useContext } from "react";
+import CartItem from "../../components/CartItem/CartItem";
+import { Context } from "../../Context";
 
 const Cart = () => {
+  const { cartItems } = useContext(Context);
+  const cartItemsElements = cartItems.map((cart) => <CartItem key={cart.id} />);
   return (
     <main className="cart-page">
       <h1>Check out</h1>
+      {cartItemsElements}
+      <p className="total-cost">Total: </p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
     </main>
   );
 };
